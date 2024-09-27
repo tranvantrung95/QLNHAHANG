86abf85cac103fa0b39aaf92b46cab2e11bff565
Tran Van Trung <tranvantrung95@gmail.com>
2024-09-27 11:56:52 +0700

    thêm giao diện chính

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';  // Import RouterModule
+import {BodyComponent} from '../layout/body/body.component';
+import {FooterComponent} from '../layout/footer/footer.component';
+import {HeaderComponent} from '../layout/header/header.component';
+import {NavigationComponent} from '../layout/navigation/navigation.component';
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,BodyComponent,FooterComponent,HeaderComponent,NavigationComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
